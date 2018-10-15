f351f0af2bae042a8e33771e4add587e06839aaa
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2018-10-15 12:20:33 -0300

    Seção 19

---
+++ email/contents.html
@@ -0,0 +1,3 @@
+<h1>Título do E-mail</h1>
+
+<p>Teste de envio de e-mail do curso de PHP 7.</p>
---
+++ email/index.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+
+
+//Tell PHPMailer to use SMTP
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "";
+
+//Password to use for SMTP authentication
+$mail->Password = "";
+
+//Set who the message is to be sent from
+$mail->SetFrom('', 'Curso PHP 7');
+
+//Set an alternative reply-to address
+$mail->addReplyTo('', 'First Last');
+
+//Set who the message is to be sent to
+$mail->addAddress('suporte@hcode.com.br', 'Suporte Hcode');
+
+//Set the subject line
+$mail->Subject = 'Testando a classe PHPMailer com Gmail';
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+
+//Attach an image file
+// $mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+
+?>
